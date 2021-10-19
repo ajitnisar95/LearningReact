2c69f98347f16def74133afea555deeb71eadd71
Ajit Nisar <30403306+ajitnisar95@users.noreply.github.com>
2021-10-19 16:39:48 +0530

    index and app.js add; initial commit; hello world.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+/*
+Note: To clear the timer, we had to name it.
+*/
